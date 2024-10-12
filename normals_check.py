5796ef2dc90b36b3acbc6da14a9c4d88813468e5
CarlijnBuis <145363583+CarlijnBuis@users.noreply.github.com>
2024-10-12 20:34:19 +0200

    Hole filling and normal orientation + dataset refinement and hole_normal_datasat

---
+++ normals_check.py
@@ -0,0 +1,42 @@
+﻿import trimesh
+import os
+
+def process_obj_file(file_path, destination_path):
+    mesh = trimesh.load(file_path, process=False)
+    if isinstance(mesh, trimesh.Trimesh):
+
+        if not mesh.is_watertight:
+            filled_mesh = mesh.fill_holes()
+
+            if isinstance(filled_mesh, trimesh.Trimesh):
+                mesh = filled_mesh
+
+        mesh.fix_normals()
+
+        os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+
+        mesh.export(destination_path)
+        print(f"Processed and saved: {destination_path}")
+    else:
+        print(f"Warning: {file_path} is not a valid Trimesh object and will be skipped.")
+
+def process_directory(source_folder, target_folder):
+
+    for root, dirs, files in os.walk(source_folder):
+        for file in files:
+            if file.endswith(".obj"):
+                file_path = os.path.join(root, file)
+
+                relative_path = os.path.relpath(file_path, source_folder)
+                destination_path = os.path.join(target_folder, relative_path)
+
+                print(f"Processing {file_path}...")
+                process_obj_file(file_path, destination_path)
+                print(f"Finished processing {destination_path}.")
+
+if __name__ == "__main__":
+    source_folder = "refined_dataset"
+
+    target_folder = "hole_normal_dataset"
+
+    process_directory(source_folder, target_folder)
