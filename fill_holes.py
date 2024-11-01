bb1e4b4a460a999815251c5d1bad9d46569b5454
Jesse Rosendahl <jesse@jessar.nl>
2024-11-01 19:38:49 +0100

    Fix holes

--- fill_holes.py
+++ fill_holes.py
@@ -1,34 +1,21 @@
-import open3d as o3d
+from vedo import *
 import numpy as np
 import os
 from pathlib import Path
 
 
-def fill_holes(mesh: o3d.geometry.TriangleMesh):
-	try:
-		half_edge_mesh : o3d.geometry.HalfEdgeTriangleMesh = o3d.geometry.HalfEdgeTriangleMesh.create_from_triangle_mesh(mesh)
-		vertices = np.asarray(mesh.vertices)
-		boundaries = half_edge_mesh.get_boundaries()
-		centroids = list(map(lambda x: np.mean(vertices[x],axis=0),boundaries))
-		triangles = np.asarray(mesh.triangles)
-		for (i,c) in enumerate(centroids):
-			vertices = np.vstack([vertices,c])
-			boundary = np.asarray(boundaries[i])
-			new_triangles = np.vstack([boundary,np.roll(boundary,1),np.repeat(len(vertices)-1,len(boundary))]).T
-			triangles = np.vstack([triangles,new_triangles])
-		
-		mesh.vertices = o3d.utility.Vector3dVector(vertices)
-		mesh.triangles = o3d.utility.Vector3iVector(triangles.astype(np.int32))
-		print(mesh.is_watertight())
-	except:
-		pass
-
+def fill_holes(mesh: Mesh):
+	mesh.compute_normals().clean()
+	holes = [x.triangulate() for x in mesh.boundaries().join_segments()]
+	mesh = merge(mesh,holes).clean().compute_normals()
+	
 	return mesh
 
+
 def process_obj_file(file_path, destination_path):
-	original_mesh = o3d.io.read_triangle_mesh(file_path)
+	original_mesh = Mesh(file_path)
 	mesh = fill_holes(original_mesh)
-	o3d.io.write_triangle_mesh(destination_path, mesh)
+	file_io.save(mesh,destination_path)
 
 def process_directory(source_folder, target_folder):
     for root, dirs, files in os.walk(source_folder):
